2bfcc71ed1e96b1c194c45a97c80ea0090f3a4d8
Elvonia <60554110+Elvonia@users.noreply.github.com>
2023-05-07 16:10:56 -0400

    Initial Commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using MelonLoader;
+using EventSkinMod;
+
+[assembly: MelonInfo(typeof(EventSkinClass), "Event Skin and Cosmetic Unlocker", "1.0.0", "Kalico")]
+[assembly: MelonGame("Backrooms", "Inside the Backrooms")]
---
+++ EventSkinClass.cs
@@ -0,0 +1,44 @@
+﻿using Il2Cpp;
+using HarmonyLib;
+using MelonLoader;
+using System.Collections.ObjectModel;
+
+namespace EventSkinMod
+{
+    public class EventSkinClass : MelonMod
+    {
+        [HarmonyPatch(typeof(SkinManager), "CheckIfSkinUnlocked")]
+        static class SkinPatch
+        {
+            public static bool Prefix(SkinNameID skin, ref bool __result)
+            {
+                if (skin == SkinNameID.HALLOWEEN_SCARECROW || skin == SkinNameID.VAMPIRE_GIRL)
+                {
+                    __result = true;
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+        }
+        [HarmonyPatch(typeof(SkinManager), "CheckIfCosmeticUnlocked")]
+        static class CosmeticPatch
+        {
+            public static bool Prefix(CosmeticNameID cosmeticNameID, ref bool __result)
+            {
+                if (cosmeticNameID == CosmeticNameID.DEER_MASK || cosmeticNameID == CosmeticNameID.SNOWMAN_MASK || cosmeticNameID == CosmeticNameID.ELF_BUDDY || cosmeticNameID == CosmeticNameID.CHRISTMAS_HAT)
+                {
+                    __result = true;
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
